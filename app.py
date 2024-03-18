2abf0939ce5f2980915de5681316468b8d850f2b
gbtwld <gbtwld@gmail.com>
2024-03-18 17:20:17 +0900

    Fix dir

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
-from src.cam import Cam
+import cv2 as cv
+from datetime import datetime
+from src.yolo import checkPerson
 
-Cam()
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+    print("camera open failed")
+    exit()
+while True:
+    ret, img = cap.read()
+    if not ret:
+        print("Can't read camera")
+        break
+
+    cv.imshow('PC_camera', img)
+    if checkPerson(img):
+        cv.imwrite('imgs/{}.png'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')), img)
+    cv.waitKey(1)
+
+cap.release()
+cv.destroyAllWindows()
